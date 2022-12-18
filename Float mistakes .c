46b28275bd396bbfcd591ee9531202cf7b7ef216
Hosam_Abdallah <115761233+HosamAbdallah@users.noreply.github.com>
2022-12-18 07:11:21 +0200

    Update Float mistakes .c

--- Float mistakes .c
+++ Float mistakes .c
@@ -2,7 +2,7 @@
 int main()
 {
     float c;
-    c =(float)5 / 9; // 5.f or 5.0 or (float)5...lazem a3ml casting 3shan lw 2asam 5/9...(int/int)
+    c =(float)5 / 9; // 5.f or 5.0 or (float)5...I must create an (Esplicit_casting)  before the divid part happened 5/9...(int/int)
     printf("c = %f\n",c);
 	return 0;
 }
